519d63cf68ec28bfd5e10e38af812409c3302ff2
Rik Goldman <rikgoldman@gmail.com>
2018-06-02 11:09:56 -0400

    fixed error in __init__.py, updated requirements

--- parking/app/__init__.py
+++ parking/app/__init__.py
@@ -10,16 +10,13 @@ from config import app_config
 # db variable initialization
 db = SQLAlchemy()
 
+
+
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
-
-    return app
-
-def create_app(config_name):
-    # existing code remains
 
     # temporary route
     @app.route('/')
