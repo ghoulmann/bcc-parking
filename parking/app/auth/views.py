36fd93e052cf7e6597e764aacd2c9b2284c757a4
Rik Goldman <rikgoldman@gmail.com>
2018-06-02 17:50:56 -0400

    change Employee and employee to User and user

--- parking/app/auth/views.py
+++ parking/app/auth/views.py
@@ -12,18 +12,18 @@ from ..models import User
 def register():
     """
     Handle requests to the /register route
-    Add an employee to the database through the registration form
+    Add an user to the database through the registration form
     """
     form = RegistrationForm()
     if form.validate_on_submit():
-        employee = Employee(email=form.email.data,
+        user = User(email=form.email.data,
                             username=form.username.data,
                             first_name=form.first_name.data,
                             last_name=form.last_name.data,
                             password=form.password.data)
 
-        # add employee to the database
-        db.session.add(employee)
+        # add user to the database
+        db.session.add(user)
         db.session.commit()
         flash('You have successfully registered! You may now login.')
 
@@ -37,17 +37,17 @@ def register():
 def login():
     """
     Handle requests to the /login route
-    Log an employee in through the login form
+    Log an user in through the login form
     """
     form = LoginForm()
     if form.validate_on_submit():
 
-        # check whether employee exists in the database and whether
+        # check whether user exists in the database and whether
         # the password entered matches the password in the database
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(
                 form.password.data):
-            # log employee in
+            # log user in
             login_user(user)
 
             # redirect to the dashboard page after login
@@ -65,7 +65,7 @@ def login():
 def logout():
     """
     Handle requests to the /logout route
-    Log an employee out through the logout link
+    Log an user out through the logout link
     """
     logout_user()
     flash('You have successfully been logged out.')
