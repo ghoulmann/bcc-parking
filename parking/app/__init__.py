760afa759707b890b28404d048021c955faaa8c4
Rik Goldman <rikgoldman@gmail.com>
2018-06-02 18:32:43 -0400

    trouble with local imports

--- parking/app/__init__.py
+++ parking/app/__init__.py
@@ -16,7 +16,7 @@ login_manager = LoginManager()
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+    app.config.from_pyfile('../config.py')
 
     Bootstrap(app)
     db.init_app(app)
@@ -25,7 +25,7 @@ def create_app(config_name):
     login_manager.login_view = "auth.login"
     migrate = Migrate(app, db)
 
-    from app import models
+    from parking.app import models
 
     from .admin import admin as admin_blueprint
     app.register_blueprint(admin_blueprint, url_prefix='/admin')
